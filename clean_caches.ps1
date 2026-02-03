f86d142f8ffd4c1bb46d72c83ca35c9f7fc2f01e
tcharles <titouancharles@gmail.com>
2026-02-03 21:47:04 +0100

    clean cache ps1

---
+++ clean_caches.ps1
@@ -0,0 +1,19 @@
+$root = Get-Location
+$dirs = @(
+  "__pycache__",
+  ".hypothesis",
+  ".pytest_cache",
+  ".mypy_cache",
+  ".ruff_cache",
+  "htmlcov"
+)
+
+Get-ChildItem -Path $root -Recurse -Force -Directory |
+  Where-Object { $dirs -contains $_.Name } |
+  ForEach-Object { Remove-Item -Recurse -Force $_.FullName }
+
+Get-ChildItem -Path $root -Recurse -Force -File -Filter "*.pyc" |
+  ForEach-Object { Remove-Item -Force $_.FullName }
+
+Get-ChildItem -Path $root -Recurse -Force -File -Filter ".coverage*" |
+  ForEach-Object { Remove-Item -Force $_.FullName }
